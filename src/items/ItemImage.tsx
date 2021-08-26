8ae3e83ddbfa34cd3335df5813ffba51c9c11e9e
Marc Hughes <marc.hughes@gmail.com>
2021-08-26 09:33:38 -0400

    Better item image rendering

--- src/items/ItemImage.tsx
+++ src/items/ItemImage.tsx
@@ -1,6 +1,16 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import styled from '@emotion/styled';
 
-export const ItemImage: React.FC<{ typeId: string }> = ({ typeId }) => (
-  <Avatar src={`https://images.evetech.net/types/${typeId}/icon`} />
+const Img = styled.img(({ size }: { size: number }) => ({
+  width: size,
+  height: size,
+  padding: 3
+}));
+
+export const ItemImage: React.FC<{ typeId: string; size?: number }> = ({
+  typeId,
+  size = 32
+}) => (
+  <Img size={size} src={`https://images.evetech.net/types/${typeId}/icon`} />
 );
